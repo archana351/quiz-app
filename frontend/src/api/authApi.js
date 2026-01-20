c1ede7a8f060f2b8bd5fdb3266b3184510606552
archana351 <vodapallyarchana@gmail.com>
2026-01-20 21:04:10 +0530

    updated backend url

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || 'https://quiz-app-backend-o4nv.onrender.com/api';;
+  process.env.REACT_APP_API_URL || 'https://quiz-app-backend-o4nv.onrender.com/api';
 
 const authApi = axios.create({
   baseURL: `${API_BASE_URL}/auth`,
